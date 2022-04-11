b5222ffaa21dffa1ef70a3c97545f82b0f2d0c79
Breadosaurus <jobwong@ucsc.edu>
2022-04-11 01:29:17 -0700

    Final Fun Time Push Time

--- src/main.js
+++ src/main.js
@@ -5,4 +5,8 @@ let config = {
     scene: [Menu, Play]
 }
 let game = new Phaser.Game(config); 
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+let borderUISize = game.config.height / 15; 
+let borderPadding = borderUISize / 3; 
 
